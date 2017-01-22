1a9dc69c8c37a7a58a9a866ace133f858ac48ec5
nathanfaucett <nathanfaucett@gmail.com>
2017-01-22 12:41:12 -0600

    clean up

--- src/rng.rs
+++ src/rng.rs
@@ -1,7 +1,36 @@
+use core::{u32, u64};
+
+
+pub static MAX_U32: u32 = u32::MAX;
+pub static MAX_U64: u64 = u64::MAX;
 
 
 pub trait Rng {
-    fn next(&mut self) -> usize;
-    fn next_f32(&mut self) -> f32;
-    fn next_f64(&mut self) -> f64;
+    fn next_u32(&mut self) -> u32;
+    fn next_u64(&mut self) -> u64;
+
+    #[cfg(target_pointer_width = "32")]
+    #[inline(always)]
+    fn next(&mut self) -> usize {
+        self.next_u32() as usize
+    }
+    #[cfg(target_pointer_width = "64")]
+    #[inline(always)]
+    fn next(&mut self) -> usize {
+        self.next_u64() as usize
+    }
+
+    #[inline(always)]
+    fn next_usize(&mut self) -> usize {
+        self.next()
+    }
+
+    #[inline(always)]
+    fn next_f32(&mut self) -> f32 {
+        self.next_u32() as f32 / (MAX_U32 as f32)
+    }
+    #[inline(always)]
+    fn next_f64(&mut self) -> f64 {
+        self.next_u64() as f64 / (MAX_U64 as f64)
+    }
 }
